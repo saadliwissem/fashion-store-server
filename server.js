a77189382c1e53d17e2e11614f45e843d72cddb9
wsaadli <wsaadli@vermeg.com>
2025-12-16 03:13:08 +0100

    deply url changed

--- server.js
+++ server.js
@@ -36,12 +36,7 @@ connectDB();
 
 // Middleware
 app.use(helmet()); // Security headers
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+app.use(cors("*"));
 // Increase payload size limit
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
